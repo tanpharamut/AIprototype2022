33bb78adc85dc330032bcacace04d7a8acea5346
tanpharamut <pharamut@kkumail.com>
2022-01-11 09:28:13 +0700

    test import

--- pyhon_script_101.py
+++ pyhon_script_101.py
@@ -1,6 +1,16 @@
+import argparse #สำหรับรับ input จากภายนอก
+import subprocess #สำหรับ run terminal command
+
+#import flask #สำหรับทำ Webapp เเละ Web service api
+
+
+
 def print_other():
         print('somthing else')
 
 
 if __name__ == "__main__":
-        print('test main  function')
+        x = 2
+        y = 3
+        print(f'calculate {x} x {y} = {x*y}')
+        
