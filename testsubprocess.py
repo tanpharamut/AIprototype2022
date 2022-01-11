fd2debccb0faba5b3dc01f643c911a90a84a503a
tanpharamut <pharamut@kkumail.com>
2022-01-11 11:58:01 +0700

    test1

--- testsubprocess.py
+++ testsubprocess.py
@@ -1,9 +1,9 @@
 import subprocess #สำหรับ รัน terminal command
 
 if __name__== "__main__":
-    subprocess.run(["ls","-l"])
-    for i in [2,5,6,9]:
-        subprocess.run(["python", "pyhon_script_101.py", "2", "--x", f"{i}", "--yval", "2"])
+    #subprocess.run(["ls","-l"])
+    #for i in [2,5,6,8]:
+       # subprocess.run(["python", "pyhon_script_101.py", "2", "--x", f"{i}", "--yval", "2"])
 
 
     ## use output of subprocess
@@ -13,7 +13,7 @@ if __name__== "__main__":
 
 
     ##HW ให้ print เฉพาะ ตัวเลขผลลัพธ์การคูณ
-    for i in [2,5,6,9]:
+    for i in [2,5,6,8]:
         process = subprocess.Popen(["python", "pyhon_script_101.py", "2", "--x", f"{i}", "--yval", "2"]
         ,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         out, err = process.communicate()
