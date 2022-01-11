153d15681e18bd11d2e9a550fe77e837bbb4f381
tanpharamut <pharamut@kkumail.com>
2022-01-11 12:13:58 +0700

    test1

--- testsubprocess.py
+++ testsubprocess.py
@@ -15,6 +15,6 @@ if __name__== "__main__":
     ##HW ให้ print เฉพาะ ตัวเลขผลลัพธ์การคูณ
     for i in [2,5,6,8]:
         process = subprocess.Popen(["python", "pyhon_script_101.py", "2", "--x", f"{i}", "--yval", "2",]
-        ,type=int,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        ,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         out, err = process.communicate()
-        print(out)
+        print(out(2))
