803015023d1b5110ff26406484e4246c00784b7f
tanpharamut <pharamut@kkumail.com>
2022-01-11 11:25:49 +0700

    test subprocess function

--- testsubprocess.py
+++ testsubprocess.py
@@ -3,4 +3,4 @@ import subprocess #สำหรับ รัน terminal command
 if __name__== "__main__":
     subprocess.run(["ls","-l"])
     for i in [2,5,6,9]:
-        subprocess.run(["python", "python pyhon_script_101.py", "8", "--x", f"{i}", "--yval", "8"])
+        subprocess.run(["python", "pyhon_script_101.py", "8", "--x", f"{i}", "--yval", "8"])
