64c5ae3c175418ed1f97ff346c6b4c889f54699a
tanpharamut <pharamut@kkumail.com>
2022-01-11 11:36:51 +0700

    test output of suprocess

--- testsubprocess.py
+++ testsubprocess.py
@@ -4,3 +4,9 @@ if __name__== "__main__":
     subprocess.run(["ls","-l"])
     for i in [2,5,6,9]:
         subprocess.run(["python", "pyhon_script_101.py", "8", "--x", f"{i}", "--yval", "8"])
+
+
+    ## use output of subprocess
+    pro = subprocess.Popen(["ls","-l"],stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = Por.communicate()
+    print(out)    
