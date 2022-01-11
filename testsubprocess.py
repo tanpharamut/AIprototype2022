b389545f6bfb0c80b8ba3147fba577f95e2a846c
tanpharamut <pharamut@kkumail.com>
2022-01-11 12:28:54 +0700

    test1

--- testsubprocess.py
+++ testsubprocess.py
@@ -17,5 +17,5 @@ if __name__== "__main__":
         process = subprocess.Popen(["python", "pyhon_script_101.py", "2", "--x", f"{i}", "--yval", "2",]
         ,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         out, err = process.communicate()
-        text = str(out), "utf=8")
-        print(text[22:28])
+        text = str(out, "utf-8")
+        print(text[24:27])
