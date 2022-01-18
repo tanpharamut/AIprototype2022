838a7b395ff5d9cd567c306e541fcbaaef500d73
tanpharamut <pharamut@kkumail.com>
2022-01-18 12:56:57 +0700

    flaskwebapp

--- testflask.py
+++ testflask.py
@@ -24,9 +24,9 @@ def request_detail():
     return json_data
 
 ##webapp
-    @app.route("/home")
-    def home():
+@app.route("/home")
+def home():
     return render_template("home.html")
-    
+
 if __name__ == "__main__":
     app.run()#host=0.0.0.0,port=5001
