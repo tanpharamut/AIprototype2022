704c25140ee5f47426e16cf76e4199883caa4eed
tanpharamut <pharamut@kkumail.com>
2022-01-25 12:04:47 +0700

    inputtowebpage

--- testflask.py
+++ testflask.py
@@ -1,3 +1,5 @@
+from crypt import methods
+from unittest import result
 from flask import Flask, request, render_template 
 import json
 
@@ -24,9 +26,24 @@ def request_detail():
     return json_data
 
 ##webapp
-@app.route("/home")
+@app.route("/home", methods = ['POST','GET'])
 def home():
-    return render_template("home.html",name='Tan')
+    if request.method == "POST":
+       # getting input with name = fname in HTML form
+        first_name = request.form.get("fname")
+       # getting input with name = lname in HTML form 
+        last_name = request.form.get("lname") 
+        return render_template("home.html",name = f"{first_name} {last_name}")
 
+    return render_template("home.html",name = 'Tan')
+
+@app.route("/home2", methods = ['POST','GET'])
+def home2():
+    if request.method == "POST": 
+        ratio_choice = request.form.get("3Dcar")
+        return render_template("home.html", ratio = f"{ratio_choice}")
+
+    return render_template("home.html", ratio= '99th')
+    
 if __name__ == "__main__":
     app.run()#host=0.0.0.0,port=5001
